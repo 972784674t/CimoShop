cb578369c36a220acf332a8430a4d83a07fcbe34
972784674t <972784674@qq.com>
2020-06-10 11:25:50 +0800

    v1.0.9 修改编码规范性

--- app/src/main/java/com/example/cimoshop/ui/shopcat/ShopCatFragment.java
+++ app/src/main/java/com/example/cimoshop/ui/shopcat/ShopCatFragment.java
@@ -47,7 +47,6 @@ public class ShopCatFragment extends Fragment {
     private MaterialToolbar toolbar;
     private RecyclerView shopCarRecyclerView;
 
-
     /**
      * 数据源
      */
@@ -62,38 +61,101 @@ public class ShopCatFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
         View root = inflater.inflate(R.layout.fragment_shopcat, container, false);
-
-        toolbar = root.findViewById(R.id.shopCatToobar);
-
-        //状态栏文字透明
-        UITools.makeStatusBarTransparent(getActivity());
-
-        //修复标题栏与状态栏重叠
-        UITools.fitTitleBar(getActivity(),toolbar);
-
-        //购物车数据初始化
-        USER_NAME = SharedPrefsTools.getInstance(getActivity().getApplication()).getUserInfo().getLogin();
-
-        SHOP_CAR_ITEM_LIST = UserDAO.getInstance(getContext()).getShopCarList(USER_NAME);
-
-        shopCarRecyclerView = root.findViewById(R.id.shopCarRecyclerView);
-        shopCarRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        shopCarAdapter = new ShopCarAdapter();
-        shopCarAdapter.setEmptyView(initEmptyView());
-        shopCarAdapter.setDiffNewData(SHOP_CAR_ITEM_LIST);
-
-
+        initViewAndDataSource(root);
         return root;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        initShopCarRecycleView();
+    }
 
-        //拖拽事件监听
-        OnItemDragListener ItemDragListener = new OnItemDragListener() {
+    /**
+     * 初始化视图控件和数据源
+     * @param root
+     */
+    private void initViewAndDataSource(View root) {
+        toolbar = root.findViewById(R.id.shopCatToobar);
+        //状态栏文字透明
+        UITools.makeStatusBarTransparent(getActivity());
+        //修复标题栏与状态栏重叠
+        UITools.fitTitleBar(getActivity(),toolbar);
+        //购物车数据初始化
+        USER_NAME = SharedPrefsTools.getInstance(getActivity().getApplication()).getUserInfo().getLogin();
+        SHOP_CAR_ITEM_LIST = UserDAO.getInstance(getContext()).getShopCarList(USER_NAME);
+        shopCarRecyclerView = root.findViewById(R.id.shopCarRecyclerView);
+    }
+
+    /**
+     * 初始化购物车RecycleView以及拖拽效果和滑动删除效果事件处理
+     */
+    private void initShopCarRecycleView() {
+        shopCarRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        shopCarAdapter = new ShopCarAdapter();
+        shopCarAdapter.setEmptyView(initEmptyView());
+        shopCarAdapter.setDiffNewData(SHOP_CAR_ITEM_LIST);
+
+        //允许拖拽并设置拖拽监听
+        shopCarAdapter.getDraggableModule().setDragEnabled(true);
+        shopCarAdapter.getDraggableModule().setOnItemDragListener(initOnItemDragListener());
+
+        //允许侧滑
+        shopCarAdapter.getDraggableModule().setSwipeEnabled(true);
+
+        //只允许从左侧滑动删除
+        shopCarAdapter.getDraggableModule().getItemTouchHelperCallback().setSwipeMoveFlags(ItemTouchHelper.START);
+        shopCarAdapter.getDraggableModule().setOnItemSwipeListener(initOnItemSwipeListener());
+        shopCarRecyclerView.setAdapter(shopCarAdapter);
+    }
+
+    /**
+     * 侧滑事件监听处理
+     * @return OnItemSwipeListener
+     */
+    private OnItemSwipeListener initOnItemSwipeListener() {
+        return new OnItemSwipeListener() {
+            @Override
+            public void onItemSwipeStart(RecyclerView.ViewHolder viewHolder, int pos) {
+                Log.d(TAG, "侧滑开始: " + pos);
+                BaseViewHolder holder = ((BaseViewHolder) viewHolder);
+            }
+
+            @Override
+            public void clearView(RecyclerView.ViewHolder viewHolder, int pos) {
+                Log.d(TAG, "侧滑结束: " + pos);
+            }
+
+            @Override
+            public void onItemSwiped(RecyclerView.ViewHolder viewHolder, int pos) {
+                Log.d(TAG, "删除视图: " + pos);
+                TextView price = viewHolder.itemView.findViewById(R.id.shopcaritemprice);
+                TextView size = viewHolder.itemView.findViewById(R.id.shopcaritemsize);
+                String imageSize = size.getText().toString();
+                String imagePrice = price.getText().toString();
+                if(UserDAO.getInstance(getContext()).delImageFromShopCar(imageSize,imagePrice)){
+                    Toast.makeText(getContext(),"删除图片id："+pos+" 成功",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getContext(),"删除图片id："+pos+" 失败",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onItemSwipeMoving(Canvas canvas, RecyclerView.ViewHolder viewHolder, float dX, float dY, boolean isCurrentlyActive) {
+                Log.d(TAG, "侧滑中->   X:" + dX +"   Y:"+ dY);
+                canvas.drawColor(ContextCompat.getColor(getContext(), R.color.colorAccent));
+            }
+        };
+    }
+
+    /**
+     * 拖拽事件监听处理
+     * @return OnItemDragListener
+     */
+    private OnItemDragListener initOnItemDragListener() {
+
+        return new OnItemDragListener() {
             @Override
             public void onItemDragStart(RecyclerView.ViewHolder viewHolder, int pos) {
                 Log.d(TAG, "开始拖拽");
@@ -144,54 +206,7 @@ public class ShopCatFragment extends Fragment {
                 }
             }
         };
-
-        //侧滑事件监听
-        OnItemSwipeListener itemSwipeListener = new OnItemSwipeListener() {
-            @Override
-            public void onItemSwipeStart(RecyclerView.ViewHolder viewHolder, int pos) {
-                Log.d(TAG, "侧滑开始: " + pos);
-                BaseViewHolder holder = ((BaseViewHolder) viewHolder);
-            }
-
-            @Override
-            public void clearView(RecyclerView.ViewHolder viewHolder, int pos) {
-                Log.d(TAG, "侧滑结束: " + pos);
-            }
-
-            @Override
-            public void onItemSwiped(RecyclerView.ViewHolder viewHolder, int pos) {
-                Log.d(TAG, "删除视图: " + pos);
-                TextView price = viewHolder.itemView.findViewById(R.id.shopcaritemprice);
-                TextView size = viewHolder.itemView.findViewById(R.id.shopcaritemsize);
-                String imageSize = size.getText().toString();
-                String imagePrice = price.getText().toString();
-                if(UserDAO.getInstance(getContext()).delImageFromShopCar(imageSize,imagePrice)){
-                    Toast.makeText(getContext(),"删除图片id："+pos+" 成功",Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getContext(),"删除图片id："+pos+" 失败",Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onItemSwipeMoving(Canvas canvas, RecyclerView.ViewHolder viewHolder, float dX, float dY, boolean isCurrentlyActive) {
-                Log.d(TAG, "侧滑中->   X:" + dX +"   Y:"+ dY);
-                canvas.drawColor(ContextCompat.getColor(getContext(), R.color.colorAccent));
-            }
-        };
-
-        //允许拖拽并设置拖拽监听
-        shopCarAdapter.getDraggableModule().setDragEnabled(true);
-        shopCarAdapter.getDraggableModule().setOnItemDragListener(ItemDragListener);
-        //允许侧滑
-        shopCarAdapter.getDraggableModule().setSwipeEnabled(true);
-        //只允许从左侧滑动删除
-        shopCarAdapter.getDraggableModule().getItemTouchHelperCallback().setSwipeMoveFlags(ItemTouchHelper.START);
-        shopCarAdapter.getDraggableModule().setOnItemSwipeListener(itemSwipeListener);
-        shopCarRecyclerView.setAdapter(shopCarAdapter);
-
-
     }
-
 
     /**
      * 初始化空列表视图
